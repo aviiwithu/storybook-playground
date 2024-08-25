a3931bde682ba71da03fb894a7a8f02e13f10768
Avinash Kumar <avinash@genus.local>
2024-08-26 00:40:22 +0530

    tailwind css

---
+++ src/components/Colors/Colors.stories.ts
@@ -0,0 +1,10 @@
+// Replace your-framework with the name of your framework
+import type { Meta } from '@storybook/react';
+ 
+import Colors from './Colors.mdx';
+ 
+const meta: Meta<typeof Colors> = {
+  component: Colors,
+};
+ 
+export default meta;
